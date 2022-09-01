8a2c4a539449483100ccdc0d1e0148fcabf1b81b
lsss <lsfirst@163.com>
2022-09-01 10:03:57 +0800

    第二次提交

--- src/main/java/com/atguigu/test/Test1.java
+++ src/main/java/com/atguigu/test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("第一次提交");
+        System.out.println("第二次提交 " );
     }
 }
