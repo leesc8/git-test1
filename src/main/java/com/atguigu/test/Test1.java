ba7b12df736c905fee4bf02b7c37e19394c34531
lsss <lsfirst@163.com>
2022-09-01 10:05:16 +0800

    第三次提交

--- src/main/java/com/atguigu/test/Test1.java
+++ src/main/java/com/atguigu/test/Test1.java
@@ -5,5 +5,6 @@ public class Test1 {
         System.out.println("新建");
         System.out.println("第一次提交");
         System.out.println("第二次提交 " );
+        System.out.println("第三次提交");
     }
 }
--- src/main/java/com/atguigu/test/Test3.java
+++ src/main/java/com/atguigu/test/Test3.java
@@ -1,8 +1,7 @@
 package com.atguigu.test;
 
-public class Test2 {
+public class Test3 {
     public static void main(String[] args) {
-        System.out.println("第二次提交");
         System.out.println("第三次提交");
     }
 }
